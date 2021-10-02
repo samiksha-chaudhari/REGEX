10624d28fedddf0227a4b85f2a8ea6c57cda1d47
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-03 00:14:08 +0530

    adding test cases

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -11,20 +11,20 @@ namespace TestProject1
         /// Tests the first name
         /// </summary>
         [TestMethod]
-       /*
+       
         public void TestFirstName()
         {
-            bool result = pattern.validateFirstName("Samiksha");
+            bool result = pattern.ValidateFirstName("Samiksha", Patterns.REGEX_FIRSTNAME);
             Assert.IsTrue(result);
         }
-       */
-
+       
+        /*
         public void FirstName_ShouldThrow_Exception()
         {
             try
             {
                 bool expected = true;
-                Assert.AreEqual(expected, pattern.validateFirstName(null));
+                Assert.AreEqual(expected, pattern.ValidateFirstName(null));
             }
             catch (UserRegiException e)
             {
@@ -32,24 +32,24 @@ namespace TestProject1
 
             }
         }
-
+        */
         /// <summary>
         /// Tests the last name
         /// </summary>
         [TestMethod]
-        /*
+        
         public void TestLastName()
         {
-            bool result1 = pattern.validateLastName("Chaudhari");
-            Assert.IsTrue(result1);
+            bool result = pattern.ValidateLastName("Mayur", Patterns.REGEX_LASTNAME);
+            Assert.IsTrue(result);
         }
-        */
+        /*
         public void LastName_ShouldThrow_Exception()
         {
             try
             {
                 bool expected = true;
-                Assert.AreEqual(expected, pattern.validateLastName(null));
+                Assert.AreEqual(expected, pattern.ValidateLastName(null));
             }
             catch (UserRegiException e)
             {
@@ -57,46 +57,46 @@ namespace TestProject1
 
             }
         }
-
+        */
 
         /// <summary>
         /// Tests the Email ID
         /// </summary>
         [TestMethod]
-        /*
+        
         public void TestEmail()
         {
-            bool result2 = pattern.validateEmailId("spchaudhari80@gmail.com");
-            Assert.IsTrue(result2);
+            bool result = pattern.ValidateEmail("spchaudhari80@gmail.com", Patterns.REGEX_EMAILID);
+            Assert.IsTrue(result);
         }
-        */
-
+        
+        /*
         public void EmailId_ShouldThrow_Exception()
         {
             try
             {
                 bool expected = true;
-                Assert.AreEqual(expected, pattern.validateEmailId(null));
+                Assert.AreEqual(expected, pattern.alidateEmailId(null));
             }
             catch (UserRegiException e)
             {
                 Assert.AreEqual("Invalid EmailId", e.Message);
 
             }
-        }
+        }*/
 
 
         /// <summary>
         /// Tests the mobile number
         /// </summary>
         [TestMethod]
-        /*
+        
         public void TestMobileNumber()
         {
-            bool result3 = pattern.validatePhoneNumber("9862583253");
-            Assert.IsTrue(result3);
+            bool result = pattern.ValidatePhoneNum("91 9123456789", Patterns.REGEX_PHONENUMBER);
+            Assert.IsTrue(result);
         }
-        */
+        /*
         public void MobileNumber_ShouldThrow_Exception()
         {
             try
@@ -109,19 +109,19 @@ namespace TestProject1
                 Assert.AreEqual("Invalid PhoneNumber", e.Message);
 
             }
-        }
+        }*/
 
         /// <summary>
         /// Test passwords
         /// </summary>
         [TestMethod]
-        /*
+        
         public void Testpassword()
         {
-            bool result4 = pattern.validatePassWord("Samik@chau80");
-            Assert.IsTrue(result4);
+            bool result = pattern.ValidatePassword("RAw@#5856", Patterns.REGEX_PASSWORD);
+            Assert.IsTrue(result);
         }
-        */
+        /*
         public void Password_ShouldThrow_Exception()
         {
             try
@@ -135,6 +135,6 @@ namespace TestProject1
 
             }
         }
-
+        */
     }
 }
